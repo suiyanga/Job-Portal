0c9007b5552bdd27e1133c966a20e1c5bf9cbac8
suiyanga <augustkinya@gmail.com>
2024-06-12 18:48:41 +0300

    updated jobs section

--- app/job/alljobs/page.tsx
+++ app/job/alljobs/page.tsx
@@ -7,7 +7,7 @@ const AllJobs = () => {
   return (
     <div className="mt-12 w-[80%] mx-auto mb-12">
       <div className="mb-[2rem]">
-        <h1 className="font-semibold">Show Result<div className="space-y-8">
+        <h1 className="font-semibold">Show Results<div className="space-y-8">
         {JobData.map((job)=>{
             return (
             <Link href={`/job/jobdetails/${job.id}`} key={job.id}>
@@ -15,7 +15,7 @@ const AllJobs = () => {
             </Link>
             );
           })}
-      </div>t({JobData.length})</h1>
+      </div>({JobData.length})</h1>
       </div>
       
     </div>
