b2db12babef071deec375a2a81def939ff63bfb1
suiyanga <augustkinya@gmail.com>
2024-06-12 18:22:13 +0300

    changes in data.ts and JobCard.tsx

--- app/job/alljobs/page.tsx
+++ app/job/alljobs/page.tsx
@@ -7,9 +7,7 @@ const AllJobs = () => {
   return (
     <div className="mt-12 w-[80%] mx-auto mb-12">
       <div className="mb-[2rem]">
-        <h1 className="font-semibold">Show Result({JobData.length})</h1>
-      </div>
-      <div className="space-y-8">
+        <h1 className="font-semibold">Show Result<div className="space-y-8">
         {JobData.map((job)=>{
             return (
             <Link href={`/job/jobdetails/${job.id}`} key={job.id}>
@@ -17,7 +15,9 @@ const AllJobs = () => {
             </Link>
             );
           })}
+      </div>t({JobData.length})</h1>
       </div>
+      
     </div>
   )
 }
